adcf94cf374132dace1837ab3cbb5632cf89639b
adaramm <ariel.mercado@ingenieria.unam.edu>
2025-12-01 11:32:46 -0600

    Add test_compile.sh for project compilation testing

---
+++ test/test_compile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "[TEST] Compilando proyecto..."
+
+gcc src/main.c -o juego 2> compile_errors.txt
+
+if [ $? -ne 0 ]; then
+    echo "[ERROR] No compila. Revisa syntax."
+    cat compile_errors.txt
+    exit 1
+fi
+
+echo "[PASS] Compilación exitosa."
