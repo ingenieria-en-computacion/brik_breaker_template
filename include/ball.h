2c277a706eb8ce3dc0125fe3acfe903afe55ebd2
adaramm <ariel.mercado@ingenieria.unam.edu>
2025-12-01 12:18:30 -0600

    Define Ball struct and modify function signatures
    Added Ball struct definition and updated function declarations.

--- include/ball.h
+++ include/ball.h
@@ -1,8 +1,16 @@
 #ifndef BALL_H
 #define BALL_H
 
+typedef struct {
+    int x, y;       // Posición
+    int dx, dy;     // Dirección
+} Ball;
+
 void ball_init();
 void ball_update();
-void ball_render();
+
+// Devuelve un puntero a la pelota global
+Ball* ball_get();
 
 #endif
+
